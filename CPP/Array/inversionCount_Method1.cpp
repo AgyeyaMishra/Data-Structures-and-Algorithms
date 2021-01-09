4947de89e5861ca7c2b148f6027b0c8a7772fd79
Agyeya Mishra <53916781+AgyeyaMishra@users.noreply.github.com>
2021-01-10 00:10:04 +0530

    Create inversionCount_Method1.cpp

---
+++ CPP/Array/inversionCount_Method1.cpp
@@ -0,0 +1,31 @@
+//C++ program to count inversion in an array 
+//Time Complexity: O(n*n)
+//Space Complexity: O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+int getInvCount(int arr[], int n)
+{
+    int inv_count = 0;
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                inv_count++;
+            }
+        }
+    }
+    return inv_count;
+}
+
+//Driver code
+int main()
+{
+    int arr[] = {1, 20, 6, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<"Number of inversions are : "<<getInvCount(arr, n);
+    
+    return 0;
+}
